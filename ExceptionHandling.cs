2dc505a962bc5f6f2a9a2fed5d8ede85bc102b1f
Hanock P Mani <140785700+HanockMani@users.noreply.github.com>
2024-07-12 21:26:54 +0530

    Add files via upload

---
+++ ExceptionHandling.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class ExceptionHandling
+{
+	public ExceptionHandling()
+	{
+        int[] ar = new int[5];
+        try
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                ar[i] = i;
+            }
+        }
+        catch (System.IndexOutOfRangeException)
+        {
+            Console.WriteLine("Range Exceeded!");
+        }
+        foreach (int i in ar)
+        {
+            Console.WriteLine(ar[i]);
+        }
+    }
+}
---
+++ Factorial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Factorial
+{
+	public Factorial(int num)
+	{
+		int fact = 1;
+		if(num == 0)
+			fact = 1;
+		else if(num > 0)
+		{
+            for (int i = 1; i <= num; i++)
+            {
+                fact = fact * i;
+            }
+        }
+		Console.WriteLine("Factorial ="+fact);
+	}
+}
