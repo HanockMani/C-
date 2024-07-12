60666c341baa2ea33b782f140e4620ef1f9bfff2
Hanock P Mani <140785700+HanockMani@users.noreply.github.com>
2024-07-12 21:26:20 +0530

    Add files via upload

---
+++ Books.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PartialClasses
+{
+    public partial class Books
+    {
+        public String authorName;
+        public String bookNo;
+        public void printDet()
+        {
+            Console.WriteLine("Author Name: " + authorName+"\nBook No: "+bookNo);
+        }
+    }
+
+    public partial class Books
+    {
+        public String bookName;
+        public String publisherName;
+        public void printPublisherDetails()
+        {
+            Console.WriteLine("Book Name: " + bookName + "\nPublisher Name: " + publisherName);
+        }
+    }
+    public partial class Books
+    {
+        static void Main(string[] args)
+        {
+           Books b1 = new Books();
+            b1.authorName = "J K Rowling";
+            b1.bookNo = "101";
+            b1.bookName = "Harry Potter";
+            b1.publisherName = "ABC Publishers";
+            b1.printDet();
+            b1.printPublisherDetails();
+        }
+    }
+}
---
+++ DayFinder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class DayFinder
+{
+    public DayFinder(int Day)
+    {
+        if (Day < 1 && Day > 7)
+            Console.WriteLine("Invalid Day!");
+        else
+        {
+            switch (Day)
+            {
+                case 1:
+                    Console.WriteLine("Day Is Sunday");
+                    break;
+                case 2:
+                    Console.WriteLine("Day is Monday");
+                    break;
+                case 3:
+                    Console.WriteLine("Day Is Tuesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Day is Wednesday");
+                    break;
+                case 5:
+                    Console.WriteLine("Day is Thursday");
+                    break;
+                case 6:
+                    Console.WriteLine("Day Is Friday");
+                    break;
+                case 7:
+                    Console.WriteLine("Day is Saturday");
+                    break;
+
+            }
+        }
+    }
+}
