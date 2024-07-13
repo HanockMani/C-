0edc0e90f90f944db36950d4a57a7f1638c0dcc7
Hanock P Mani <140785700+HanockMani@users.noreply.github.com>
2024-07-13 19:43:19 +0530

    Add files via upload

---
+++ OddOrEven.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class OddOrEven
+{
+	public OddOrEven(int num)
+	{
+		if (num % 2 == 0) Console.WriteLine("Number is Even");
+		else Console.WriteLine("Number is Odd");
+    }
+}
---
+++ mDelegate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+delegate void mDel();
+
+public class mDelegate
+{
+    public static void print()
+    {
+        Console.WriteLine("New Delhi");
+    }
+
+    public static void disp()
+    {
+        Console.WriteLine("New York");
+    }
+}
