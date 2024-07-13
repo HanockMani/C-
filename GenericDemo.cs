4ad270ca27bf792979ca4bb12b0796474f463ca2
Hanock P Mani <140785700+HanockMani@users.noreply.github.com>
2024-07-13 19:42:54 +0530

    Add files via upload

---
+++ GenericDemo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+public class GenericDemo
+{
+    public GenericDemo()
+    {
+        StringDataBase<String> obj = new StringDataBase<String>();
+        obj.Add("Hello");
+        obj.Add("World");
+        obj.addTwoNumbers(3.5, 5.7);
+        obj.printList();
+        obj.swap<int>(5, 10);
+        obj.swap<String>("Welcome", "Home");
+    }
+}
+
+public class StringDataBase<T>
+{
+    List<T> elements = new List<T>();
+
+    public void Add(T value)
+    {
+        elements.Add(value);
+    }
+
+    public void addTwoNumbers<T>(T a, T b)
+    {
+        dynamic x = a;
+        dynamic y = b;
+        T Sum = x + y;
+        Console.WriteLine(Sum);
+    }
+
+    public void printList()
+    {
+        foreach (T value in elements)
+        {
+            Console.WriteLine(value);
+        }
+    }
+
+    public void swap<T1>(T1 a, T1 b)
+    {
+        Console.WriteLine($"After swapping, a = {a}, b ={b}");
+        T1 temp = a;
+        a = b;
+        b = temp;
+        Console.WriteLine($"After swapping, a = {a}, b ={b}");
+    }
+
+}
---
+++ MathOperation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+delegate int ArithOp(int x, int y);
+
+public class MathOperation
+{
+	public static int Add(int a, int b)
+	{
+		return (a + b);
+	}
+
+	public static int sub(int a, int b)
+	{
+		return (a - b);
+	}
+}
