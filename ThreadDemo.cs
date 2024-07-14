e85b85bd98880839b6917a3f38f4ccb1c4e76f8f
Hanock P Mani <140785700+HanockMani@users.noreply.github.com>
2024-07-14 09:30:37 +0530

    Add files via upload

---
+++ ThreadDemo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+public class ThreadDemo
+{
+	public ThreadDemo()
+	{
+		Thread thread1 = new Thread(() => PrintNumbers("Thread1"));
+        Thread thread2 = new Thread(() => PrintNumbers("Thread2"));
+
+		thread1.start();
+		thread2.start();
+
+        Console.WriteLine("Main thread is continuing to run");
+
+        thread1.join();
+        thread2.join();
+
+		Console.WriteLine("Both threads have finished execution");
+
+    }
+
+    public void PrintNumbers(string threadName)
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine($"{threadName}: {i}");
+            Thread.sleep(500);
+        }
+    }
+}
+
---
+++ TypeCasting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class TypeCasting
+{
+	public TypeCasting()
+	{
+        long myNum = 15000000000L;
+        Console.WriteLine(myNum);
+
+        //Automatic casting
+        int myInt = 10;
+        double myDouble = myInt;
+        Console.WriteLine(myInt);
+
+        //DownCasting
+        myInt = (int)myDouble;
+        Console.WriteLine(myDouble);
+            
+    }
+}
