19b96a8a82c5b209507a8df274d5df4f449212e5
Hanock P Mani <140785700+HanockMani@users.noreply.github.com>
2024-07-14 09:30:19 +0530

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            new OddOrEven(3);
+            new DayFinder(6);
+            new Factorial(10);
+            new TypeCasting();
+            new ExceptionHandling();
+            new GenericDemo();
+            new AnonymousFunctionDemo();
+            */
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Delegates
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*
+            
+            ArithOp operation1 = new ArithOp(MathOperation.Add);
+            ArithOp operation2 = new ArithOp(MathOperation.sub);
+            int result1 = operation1(1200, 1000);
+            int result2 = operation2(1200, 1000);
+            Console.WriteLine("Addition = "+result1);
+            Console.WriteLine("Subtraction = " + result2);
+            
+            */
+
+            /*
+            */
+
+            mDel m1 = new mDel(mDelegate.print);
+            mDel m2 = new mDel(mDelegate.disp);
+            mDel m3 = m1 + m2;
+            mDel m4 = m2 + m3;
+            mDel m5 = m3 - m2;
+            m1();
+            m2();
+            m3();
+            m4();
+            m5();
+        }
+    }
+}
